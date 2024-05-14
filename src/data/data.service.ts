f6bff2feb98303ba3d5eb4e50b1268bc380fc2c4
Ing. Camilo Ortega <camilo.ortega@simerelectronics.com>
2024-05-14 17:14:11 -0500

    Issue#003 Update: 	*Se ha actualizado el servicio para generar datos, haciendo 	pruebas con limites y saltos para la ejecución correcta en 	el paginador de la aplicación.

--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -53,15 +53,18 @@ export class DataService {
 
       console.log('startDateTime', startDateTime);
       console.log('endDateTime', endDateTime);
-      //console.log('Server time:', new Date().toISOString());
 
       const serverTime = moment().format();
       console.log('Server time:', serverTime);
+
+      // const skip = (billData.page - 1) * billData.limit;
 
       const data = await this.dataRepository
         .createQueryBuilder('datos')
         .where("fecha >= :startDateTime", { startDateTime })
         .andWhere("fecha <= :endDateTime", { endDateTime })
+        // .skip(skip)
+        // .take(billData.limit)
         .getMany();
 
       const formattedData = data.map(item => ({
@@ -118,5 +121,80 @@ export class DataService {
       return ResponseUtil.error(400, 'Error al generar el csv', error)
     }
   }
+
+  // async generateCsvbyDate(billData: any) {
+  //   try {
+  //     console.log('billData', billData);
+
+  //     const startDateTime = billData.start + 'T' + billData.time_start + '.00Z';
+  //     const endDateTime = billData.end + 'T' + billData.time_end + '.00Z';
+
+  //     console.log('startDateTime', startDateTime);
+  //     console.log('endDateTime', endDateTime);
+  //     //console.log('Server time:', new Date().toISOString());
+
+  //     const serverTime = moment().format();
+  //     console.log('Server time:', serverTime);
+
+  //     const data = await this.dataRepository
+  //       .createQueryBuilder('datos')
+  //       .where("fecha >= :startDateTime", { startDateTime })
+  //       .andWhere("fecha <= :endDateTime", { endDateTime })
+  //       .getMany();
+
+  //     const formattedData = data.map(item => ({
+  //       ...item,
+  //       fecha: moment.tz(item.fecha, 'America/Bogota').format('YYYY-MM-DD HH:mm:ss')
+  //     }));
+
+  //     const records = formattedData.map(data => ({
+  //       id: data.id,
+  //       admin: data.admin,
+  //       id_admin: data.id_admin,
+  //       operario: data.operario,
+  //       id_operario: data.id_operario,
+  //       id_bascula: data.id_bascula,
+  //       fecha: data.fecha,
+  //       nif: data.nif,
+  //       capacidad: data.capacidad,
+  //       tara: data.tara,
+  //       peso_inicial: data.peso_inicial,
+  //       peso_final: data.peso_final,
+  //       masa_aplicada: data.masa_aplicada,
+  //       estado: data.estado,
+  //       sucursal: data.sucursal,
+  //     }))
+
+  //     const headers = [
+  //       'Id',
+  //       'Admin',
+  //       'Id_admin',
+  //       'Operario',
+  //       'Id_operario',
+  //       'Id_bascula',
+  //       'Fecha',
+  //       'Nif',
+  //       'Capacidad',
+  //       'Tara',
+  //       'Peso_inicial',
+  //       'Peso_final',
+  //       'Masa_aplicada',
+  //       'Estado',
+  //       'Sucursal',
+  //     ];
+
+  //     if (records.length < 1) {
+  //       return ResponseUtil.error(400, 'No hay datos para generar el csv')
+  //     }
+
+  //     console.log('records', records);
+
+  //     return ResponseUtil.success(200, 'Datos csv generados correctamente', { headers, records })
+
+  //   } catch (error) {
+  //     console.error(error);
+  //     return ResponseUtil.error(400, 'Error al generar el csv', error)
+  //   }
+  // }
 
 }
