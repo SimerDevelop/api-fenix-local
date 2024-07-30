4404165d26075dd823cb40ad0e41a4074f3318d9
Ing. Camilo Ortega <camilo.ortega@simerelectronics.com>
2024-07-30 08:56:08 -0500

    Issue#005 Update: 	*Se ha aumentado el tamaño del cuerpo de la solicitud 	recibida a	50MB.
    	*Se ha agregado servicio para creación de nifs con validación
    	si el nif existe se reescribe.
    
    	*Se ha agregado servicio para busqueda por nif (findOne).
    
    	*Se ha agregado método update para nifs.
    
    	*Se ha agregado método para crear multiples solicitudes de
    	creación de nifs en paralelo para aumentar la eficiencia de
    	carga de datos.

--- src/nif/nif.controller.ts
+++ src/nif/nif.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param } from '@nestjs/common';
 import { NifService } from './nif.service';
 import { Nif } from './entities/nif.entity';
 
@@ -6,9 +6,32 @@ import { Nif } from './entities/nif.entity';
 export class NifController {
   constructor(private readonly nifService: NifService) {}
 
+  @Post('create')
+  async createNIF(@Body() nifData: Nif): Promise<Nif> {
+    return this.nifService.createNIF(nifData);
+  }
+
+  @Get('getById/:id')
+  async findOne(@Param('id') id: string): Promise<any> {
+    return this.nifService.findOne(id);
+  }
+
+  @Put('update/:id')
+  async update(@Param('id') id: string, @Body() nifData: Nif): Promise<any> {
+    return this.nifService.update(id, nifData);
+  }
+
   @Get('validator')
   async validateNif(): Promise<Nif[]> {
     return this.nifService.validateNif();
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  @Post('createMultiple')
+  async createMultiple(@Body() nifData: Nif): Promise<Nif> {
+      return this.nifService.createMultiple(nifData);
   }
 
 }
--- src/nif/nif.service.ts
+++ src/nif/nif.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Nif } from './entities/nif.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -10,6 +10,165 @@ export class NifService {
   constructor(
     @InjectRepository(Nif) private nifRepository: Repository<Nif>,
   ) { }
+
+  async createNIF(nifData: Nif): Promise<any> {
+    try {
+      if (nifData) {
+        // Validar y corregir el formato de la fecha
+        const dateRegex = /^\d{4}[-/]\d{2}[-/]\d{2}$/;
+        if (!dateRegex.test(nifData.fecha_mtto)) {
+          return ResponseUtil.error(400, 'El formato de la fecha es inválido. Debe ser yyyy-mm-dd.');
+        }
+        nifData.fecha_mtto = nifData.fecha_mtto.replace(/\//g, '-');
+
+        // Validar el campo nifData.nif
+        const nifRegex = /^[^\s,]{9,}$/;
+        if (!nifRegex.test(nifData.nif)) {
+          return ResponseUtil.error(400, 'El campo NIF es inválido.');
+        }
+
+        const existingNif = await this.nifRepository
+          .createQueryBuilder('cilindros')
+          .where('cilindros.nif = :nif', { nif: nifData.nif })
+          .getOne();
+
+        if (existingNif) {
+          const updatedNif = await this.nifRepository.save({
+            ...existingNif,
+            ...nifData,
+          });
+
+          return ResponseUtil.success(
+            200,
+            'NIF actualizado exitosamente',
+            updatedNif
+          );
+        }
+
+        const newNif = this.nifRepository.create({
+          ...nifData,
+        });
+
+        const createdNif = await this.nifRepository.save(newNif);
+
+        if (createdNif) {
+          return ResponseUtil.success(
+            200,
+            'NIF creado exitosamente',
+            createdNif
+          );
+        } else {
+          return ResponseUtil.error(
+            500,
+            'Ha ocurrido un problema al crear el NIF'
+          );
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      return ResponseUtil.error(
+        500,
+        'Error al crear el NIF',
+        error.message
+      );
+    }
+  }
+
+  async findOne(nifData: string): Promise<any> {
+    try {
+      const nif = await this.nifRepository
+        .createQueryBuilder('cilindros')
+        .where('cilindros.nif = :nif', { nif: nifData })
+        .getOne();
+
+      if (nif) {
+        return ResponseUtil.success(
+          200,
+          'NIF encontrado',
+          nif
+        );
+      } else {
+        return ResponseUtil.error(
+          404,
+          'NIF no encontrado'
+        );
+      }
+    } catch (error) {
+      return ResponseUtil.error(
+        500,
+        'Error al obtener el NIF'
+      );
+    }
+  }
+
+  async update(id, nifData) {
+    try {
+      const existingNif = await this.nifRepository.findOne({
+        where: { id },
+      });
+
+      // Validar el campo nifData.nif
+      const nifRegex = /^[^\s,]{9,}$/;
+      if (!nifRegex.test(nifData.nif)) {
+        return ResponseUtil.error(400, 'El campo NIF es inválido.');
+      }
+
+      if (!existingNif) {
+        return ResponseUtil.error(
+          400,
+          'NIF no encontrado'
+        );
+      }
+
+      const updatedNif = await this.nifRepository.save({
+        ...existingNif,
+        ...nifData,
+      });
+
+      return ResponseUtil.success(
+        200,
+        'NIF actualizada exitosamente',
+        updatedNif
+      );
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        return ResponseUtil.error(
+          404,
+          'NIF no encontrado'
+        );
+      }
+      return ResponseUtil.error(
+        500,
+        'Error al actualizar el NIF'
+      );
+    }
+  }
+
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  async createMultiple(data: any): Promise<any> {
+    const chunkSize = 500;
+    const createdNifs = [];
+
+    for (let i = 0; i < data.length; i += chunkSize) {
+      const chunk = data.slice(i, i + chunkSize);
+      const promises = chunk.map((item: any) => this.createNIF(item));
+      const responses = await Promise.all(promises);
+
+      const successfulNifs = responses
+        .filter(response => response.statusCode === 200)
+        .map(response => response.data.id);
+
+      createdNifs.push(...successfulNifs);
+    }
+
+    return ResponseUtil.success(
+      200,
+      'NIFS creados exitosamente',
+      createdNifs
+    );
+  }
 
   async validateNif(): Promise<any> {
     try {
